4e0eec4b43889d9d49c469eb8dcf6cc228b5ed4c
Jullia Saad <julliasaad01@gmail.com>
2017-10-26 15:51:46 -0200

    Comenta user by id

--- app/controller/user.js
+++ app/controller/user.js
@@ -43,6 +43,7 @@ const UserController = {
     /*Update*/
     var _id = req.body._id;
     if (_id) {
+      console.log("tenho id");
       user.findByIdAndUpdate(_id, req.body).exec()
         .then(
           function(user) {
